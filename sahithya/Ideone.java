1c1958cfee923d6b2d1af7bd36fcbad01a1bf5ee
Neppalasahithya <41731065+Neppalasahithya@users.noreply.github.com>
2018-07-26 16:12:11 +0530

    Create Ideone.java

---
+++ sahithya/Ideone.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n=3;
+	
+		if(n>0)
+		{
+			System.out.print("value is positive");
+		}
+		else if(n<0)
+		{
+			System.out.println("value is negative");
+			}
+		else if(n==0)
+		{
+			System.out.println("value is zero");
+		}
+	}
+}
