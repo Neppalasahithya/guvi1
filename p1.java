0128765e58546dd5a434b7370165a65b6884e746
Neppalasahithya <41731065+Neppalasahithya@users.noreply.github.com>
2023-10-10 11:29:49 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -3,6 +3,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
+// this is for testing
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
